d677abd9fb031b2071c0d3d7aa7720b91443aee0
febin123 <febsep96@gmail.com>
2024-03-13 02:30:15 +0530

    added useStyles in Material UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import Coinpage from './Pages/Coinpage';
 import Header from './Components/Header';
 
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
 const App = () => {
+  const classes=useStyles()
   return (
     <div>
       <BrowserRouter>
+      <div className={classes.App}>
       <Header/>
       <Routes>
         
-        <Route path='/' element={<Homepage/>}/>
+        <Route path='/' element={<Homepage/>} exact/>
         <Route path='/coins/:id' element={<Coinpage/>}/>
       
       </Routes>
+      </div>
       </BrowserRouter>
     </div>
   )
