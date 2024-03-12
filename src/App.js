d7c476ab663c18a67627efd99c6cc390c82830db
febin123 <febsep96@gmail.com>
2024-03-13 02:20:42 +0530

    created page routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-function App() {
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import Coinpage from './Pages/Coinpage';
+import Header from './Components/Header';
+
+const App = () => {
   return (
-    <div className="App">
-    
-     HEllo World
+    <div>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/coins/:id' element={<Coinpage/>}/>
+      
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
